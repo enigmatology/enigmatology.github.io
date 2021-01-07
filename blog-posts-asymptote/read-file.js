d8ae9a55a8728a9bb02ac2e23b6668cf6a840ceb
wgvozdjak <70719982+wgvozdjak@users.noreply.github.com>
2021-01-07 10:56:11 -0800

    Update read-file.js

--- blog-posts-asymptote/read-file.js
+++ blog-posts-asymptote/read-file.js
@@ -1,6 +1,6 @@
 function addFile() {
   var client = new XMLHttpRequest();
-  client.open('GET', "test-post.txt");
+  client.open('GET', "https://wgvozdjak.github.io/blog-posts-asymptote/test-post.txt");
   client.onreadystatechange = function() {
   	document.getElementById("blog-post").textContent = client.responseText;
   }
