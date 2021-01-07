ff6a6e5e401e7f49f8f7b99ae9a544d87192493c
wgvozdjak <70719982+wgvozdjak@users.noreply.github.com>
2021-01-07 10:05:19 -0800

    Update read-file.js

--- blog-posts-asymptote/read-file.js
+++ blog-posts-asymptote/read-file.js
@@ -1,13 +1,14 @@
 function readFile(file) {
   const fs = require('fs') 
 
-  fs.readFile('Input.txt', (err, data) => { 
+  fs.readFile(file, (err, data) => { 
       if (err) throw err; 
 
       return data.toString()); 
   }) 
 }
 
-function addFile(fileContents) {
+function addFile() {
+  var fileContents = readFile("text-post.txt");
   document.getElementById("blog-post").textContent = fileContents;
 }
