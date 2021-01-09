e91c312a0538a37ee55b62af2237a4f0861a5baf
wgvozdjak <70719982+wgvozdjak@users.noreply.github.com>
2021-01-08 16:17:29 -0800

    Update read-file.js

--- blog-posts-asymptote/read-file.js
+++ blog-posts-asymptote/read-file.js
@@ -10,10 +10,10 @@ var f = (function(){
           para = document.createElement("div");
           para.setAttribute("id", i+"");
           para.innerHTML = this.responseText;
-		  document.body.appendChild(para);
+	  document.body.appendChild(para);
         }
       };
-      xhttp.open("GET", "https://wgvozdjak.github.io/blog-posts-asymptote/" + i + ".txt", true);
+      xhttp.open("GET", "https://wgvozdjak.github.io/blog-posts-asymptote/" + i + ".txt", false);
       xhttp.send();
     })(i, para);
   }
