4d75865b16997c59c0c780507a6beb3ef5b7df24
wgvozdjak <70719982+wgvozdjak@users.noreply.github.com>
2021-01-11 11:41:05 -0800

    Update read-file.js

--- blog-posts-asymptote/read-file.js
+++ blog-posts-asymptote/read-file.js
@@ -1,26 +1,3 @@
-/*window.onload = function addFile() {
-var xhttp;
-var para;
-var f = (function(){
-  for (var i = 1; i <= 3; i++) {
-    (function(i, para){
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          para = document.createElement("div");
-          para.setAttribute("id", i+"");
-          para.innerHTML = this.responseText;
-	  document.body.appendChild(para);
-        }
-      };
-      xhttp.open("GET", "https://wgvozdjak.github.io/blog-posts-asymptote/" + i + ".txt", false);
-      xhttp.send();
-    })(i, para);
-  }
-})();
-}
-*/
-
 window.onload = function addFile() {
 var xhttp;
 var para, header, content;
@@ -46,6 +23,10 @@ var f = (function(){
           header.innerHTML = splitText[0];
           
           remainingText = text.slice(splitText[0].length+2, text.length);
+          
+          while (remainingText.includes("\n"))  {
+            remainingText = remainingText.replace("\n", "<br>");
+          }
           
           content.innerHTML = remainingText;
 		  
