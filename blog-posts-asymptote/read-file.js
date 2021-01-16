c29d6f3dd0f61fa0af628b4956b20e5426c20f1e
wgvozdjak <70719982+wgvozdjak@users.noreply.github.com>
2021-01-15 22:26:47 -0800

    Update read-file.js

--- blog-posts-asymptote/read-file.js
+++ blog-posts-asymptote/read-file.js
@@ -1,4 +1,4 @@
-window.onload = function addFile() {
+/*window.onload = function addFile() {
 var xhttp;
 var para, header, content;
 var text, splitText, remainingText;
@@ -63,4 +63,167 @@ function convertbbCode(orig) {
     converted = converted.replace("[/u]", "</u>");
   }
   return converted;
+}*/
+
+var hides = [];
+var posts = [];
+var iterations = 0;
+var NUMBEROFPOSTS = 3;
+
+for (var i = 0; i < NUMBEROFPOSTS - 1; i++) {
+  posts.push(0);
+}
+
+window.onload = function addFile() {
+
+var xhttp;
+
+var f = (function(){
+  for (var i = 1; i <= NUMBEROFPOSTS; i++) {
+    
+    (function(i){
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          posts[i-1] = this.responseText;
+          addcontent(posts, hides);
+          
+        }
+      };
+      xhttp.open("GET", "https://wgvozdjak.github.io/blog-posts-asymptote/" + i + ".txt", true);
+      xhttp.send();
+    })(i);
+  }
+})();
+}
+
+function addcontent(posts, hides) {
+  var para, header, content;
+  var text, splitText, remainingText;
+  var finished = true;
+  for (var i = 0; i < posts.length; i++) {
+    if (posts[i] === 0) {
+      finished = false;
+    }
+  }
+  
+  if (finished === true) {
+    for (var i = 0; i < posts.length; i++) {
+      element = document.getElementById(i+"");
+      if (element == null) {
+
+        para = document.createElement("div");
+        para.setAttribute("id", i+"");
+        header = document.createElement("div");
+        header.setAttribute("class", "post-header");
+        content = document.createElement("div");
+        content.setAttribute("class", "post-content");
+        document.body.appendChild(para);
+        document.getElementById(i+"").appendChild(header);
+        document.getElementById(i+"").appendChild(content);
+
+        text = posts[i];
+        splitText = text.split("\n");
+
+        header.innerHTML = splitText[0];
+
+        remainingText = text.slice(splitText[0].length+2, text.length);
+
+        while (remainingText.includes("\n"))  {
+          remainingText = remainingText.replace("\n", "<br>");
+        }
+
+        remainingText = convertbbCode(remainingText, i, []);
+
+        content.innerHTML = remainingText;
+        
+        
+        
+      }
+    }
+  }
+  
+  
+  if (finished === true) {
+  document.getElementById("b").textContent = document.getElementById(0).innerHTML;
+    for (let i = 1; i <= hides.length; i++) {
+      var element = document.getElementById(i + "hide");
+      
+      /*if (i === 1) {
+        document.getElementById("a").textContent = document.getElementById("1hide-content").classList;
+      }*/
+      element.addEventListener("click", function() {
+      //shown (hidden)
+                                          document.getElementById("a").textContent = String(document.getElementById(i + "thing").innerHTML)[31] + "B";
+                                          //shown -> hidden
+                                          toggleshow(i);
+                                          //document.getElementById("1hide-content").classList.remove("shown");
+                                          //document.getElementById("1hide-content").classList.add("hidden");
+                                        });
+    }
+  }
+}
+  
+
+function convertbbCode(orig, i, currentcontents) {
+  var converted = orig;
+  
+  var begbeg, endbeg, endend;
+  var hidelabel, hidecontent, replacetext;
+  var slicedhide;
+  
+  while (converted.includes("[b]")) {
+    converted = converted.replace("[b]", "<b>");
+  }
+  while (converted.includes("[/b]")) {
+    converted = converted.replace("[/b]", "</b>");
+  }
+  while (converted.includes("[u]")) {
+    converted = converted.replace("[u]", "<u>");
+  }
+  while (converted.includes("[/u]")) {
+    converted = converted.replace("[/u]", "</u>");
+  }
+  
+  loop = i;
+  while (converted.includes("[hide=")) {
+    begbeg = converted.indexOf("[hide=");
+    endend = converted.indexOf("[/hide]") + 7;
+    
+    slicedhide = converted.slice(begbeg, endbeg);
+    
+    endbeg = slicedhide.indexOf("[/hide]")
+    hidelabel = slicedhide.slice(6, slicedhide.indexOf("]"));
+    hidecontent = slicedhide.slice(slicedhide.indexOf("]") + 1, endbeg);
+    
+    slicedhide = converted.slice(begbeg, endbeg)
+    replacetext = "<button id=\"" + (hides.length+1) + "hide\">" + hidelabel + "</button><div id=\"" + (hides.length + 1) + "thing\"><div id=\"" + (hides.length+1) + "hide-content\" class=\"shown\">" + hidecontent + "</div></div>";
+    
+    converted = converted.replace(converted.slice(begbeg, endend), replacetext);
+    i += 1;
+    hides.push(hides.length+1);
+    
+  }
+  return converted;
+}
+
+function toggleshow(num) {
+  
+  var element = document.getElementById(num + "hide-content");
+  //document.getElementById("c").innerHTML = num + " " + Math.random();
+  var thing = Math.random();
+  document.getElementById("c").innerHTML = element.classList;// + " " + thing;
+  if (element.classList.contains("hidden")) {
+    element.classList.remove("hidden");
+    element.classList.add("shown");
+    thing += 1;
+  }
+  else if (element.classList.contains("shown")) {
+    element.classList.remove("shown");
+    element.classList.add("hidden");
+    thing += 1;
+  }
+  document.getElementById("d").innerHTML = element.classList + " " + thing;
+  
+//hidden is blue, shown is orange
 }
