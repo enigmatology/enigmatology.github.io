f9c6d6405cfcc26fce6f5ffaa4380f453f1be4f6
enigmatology <81991228+enigmatology@users.noreply.github.com>
2021-04-05 13:52:20 -0700

    Update addnavbar.js

--- file-snippets/addnavbar.js
+++ file-snippets/addnavbar.js
@@ -1,7 +1,7 @@
 var navbardiv = document.getElementById("navbar");
 (function () {
   var xhttp = new XMLHttpRequest();	
-  xhttp.open("GET", "https://wgvozdjak.github.io/file-snippets/navbar", true);
+  xhttp.open("GET", "https://enigmatology.github.io/file-snippets/navbar", true);
   xhttp.send();
 
   xhttp.onreadystatechange = function() {
