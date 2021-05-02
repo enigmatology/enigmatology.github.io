bdbaa2cb9957e4acbf536b8828e363d9c563a125
wgvozdjak <70719982+wgvozdjak@users.noreply.github.com>
2021-05-02 16:00:28 -0700

    Update mathjax-init.js

--- javascript/mathjax-init.js
+++ javascript/mathjax-init.js
@@ -1,42 +1,17 @@
-pagejax();
-
-function pagejax() {
-  let jax = document.createElement("script");
-  jax.setAttribute("id", "pagejax-init");
-  jax.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js";
-  document.head.appendChild(jax);
-}
-
 function startprintjax() {
-  let oldjax = document.getElementById("pagejax-init");
-  oldjax.remove();
-  
-  let newjax = document.createElement("script");
-  newjax.setAttribute("id", "pagejax-init");
-  newjax.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js";
-  document.head.appendChild(newjax);
-  MathJax.typeset();
+  MathJax.Hub.Queue(["setRenderer", MathJax.Hub, "SVG"]);
+  MathJax.Hub.Queue(["Rerender", MathJax.Hub]);
 }
 
 function endprintjax() {
-  let oldjax = document.getElementById("pagejax-init");
-  oldjax.remove();
-  
-  let newjax = document.createElement("script");
-  newjax.setAttribute("id", "pagejax-init");
-  newjax.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js";
-  document.head.appendChild(newjax);
-  MathJax.typeset();
+  MathJax.Hub.Queue(["setRenderer", MathJax.Hub, "CommonHTML"]);
+  MathJax.Hub.Queue(["Rerender", MathJax.Hub]);
 }
 
 document.body.onbeforeprint = function() {
-  alert("starting print");
   startprintjax();
-  
 }
 
 document.body.onafterprint = function() {
-  alert("ending print");
   endprintjax();
-  
 }
