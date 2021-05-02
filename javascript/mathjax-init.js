557b05ac117d7cae2ace2ff1543ca3158bedc3ca
wgvozdjak <70719982+wgvozdjak@users.noreply.github.com>
2021-05-02 14:42:41 -0700

    added print-friendly mathjax initilization

---
+++ javascript/mathjax-init.js
@@ -0,0 +1,36 @@
+pagejax();
+
+function pagejax() {
+  let jax = document.createElement("script");
+  jax.setAttribute("id", "pagejax-init");
+  jax.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js";
+  document.head.appendChild(jax);
+}
+
+function startprintjax() {
+  let oldjax = document.getElementById("pagejax-init");
+  oldjax.remove();
+  
+  let newjax = document.createElement("script");
+  newjax.setAttribute("id", "pagejax-init");
+  newjax.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js";
+  document.head.appendChild(newjax);
+}
+
+function endprintjax() {
+  let oldjax = document.getElementById("pagejax-init");
+  oldjax.remove();
+  
+  let newjax = document.createElement("script");
+  newjax.setAttribute("id", "pagejax-init");
+  newjax.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js";
+  document.head.appendChild(newjax);
+}
+
+body.onbeforeprint = function() {
+  startprintjax();
+}
+
+body.onafterprint = function() {
+  endprintjax();
+}
