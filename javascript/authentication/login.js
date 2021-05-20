59e62733ea7a7ad8d70806046fda3a9f3b09803e
wgvozdjak <70719982+wgvozdjak@users.noreply.github.com>
2021-05-20 11:13:16 -0700

    removed firebase init

--- javascript/authentication/login.js
+++ javascript/authentication/login.js
@@ -1,18 +1,3 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-  apiKey: "AIzaSyDRtckGECYdhgZAGTKUpO60NT5NQAsnFd0",
-  authDomain: "enigmatology-95018.firebaseapp.com",
-  projectId: "enigmatology-95018",
-  storageBucket: "enigmatology-95018.appspot.com",
-  messagingSenderId: "563230550022",
-  appId: "1:563230550022:web:109ff2a5bb073f5f237b87",
-  measurementId: "G-8PKK0H9346"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-
 function toggledisplay(element) {
   if (element.classList.contains("hidden")) {
     element.classList.remove("hidden");
