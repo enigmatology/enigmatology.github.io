01508ea2c025968ff39c73bbf30235f005cf964b
wgvozdjak <70719982+wgvozdjak@users.noreply.github.com>
2021-05-21 11:35:38 -0700

    switched order of username and vertical separator

--- javascript/authentication/general.js
+++ javascript/authentication/general.js
@@ -17,12 +17,12 @@ function showusername() {
   let auth = firebase.auth();
   let headdiv = document.getElementById("auth");
   auth.onAuthStateChanged(function(user) {
-    headdiv.innerHTML = "<div id=\"vertical-separator\"></div>";
     if (user) {
       headdiv.innerHTML += "<div id=\"logged-in\">" + user.displayName + "</div>";
     }
     else {
       headdiv.innerHTML += "<div id=\"logged-in\">" + "Currently logged out" + "</div>";
     }
+    headdiv.innerHTML = "<div id=\"vertical-separator\"></div>";
   });
 }
