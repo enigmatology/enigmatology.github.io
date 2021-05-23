708dc8861d9a6994adee36e4939e1c2dea4c7170
wgvozdjak <70719982+wgvozdjak@users.noreply.github.com>
2021-05-22 17:51:25 -0700

    added another div
    this is getting tiring

--- javascript/authentication/general.js
+++ javascript/authentication/general.js
@@ -18,7 +18,7 @@ function showusername() {
   let headdiv = document.getElementById("auth");
   auth.onAuthStateChanged(function(user) {
     if (user) {
-      headdiv.innerHTML = "<div id=\"user-wrapper\"><div id=\"logged-in\">" + user.displayName + "</div><div id=\"logout-dropdown\"><button id=\"logout-btn\" onclick=\"logout()\">Sign out</button></div></div>";
+      headdiv.innerHTML = "<div id=\"user-wrapper\"><div id=\"user-compress-wrapper\"><div id=\"logged-in\">" + user.displayName + "</div><div id=\"logout-dropdown\"><button id=\"logout-btn\" onclick=\"logout()\">Sign out</button></div></div></div>";
     }
     else {
       headdiv.innerHTML = "<div id=\"logged-in\">" + "<a id=\"login-register\" href=\"https://enigmatology.github.io/login\">Login/Register</a>" + "</div>";
